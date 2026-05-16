c2d189d66ec6a59444118747f824c22a82be2aee
Nethsalee <nethsalee@gmail.com>
2026-05-17 01:10:33 +0530

    Add MySessions and SessionFeedback pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Sessions from './pages/Sessions';
-import BookingConfirmed from './pages/BookingConfirmed';
+import SessionBooking from './pages/Sessions/SessionBooking';
+import BookingConfirmed from './pages/Sessions/BookingConfirmed';
+import SessionFeedback from './pages/Sessions/SessionFeedback';
+import MySessions from './pages/Sessions/MySessions';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Sessions />} />
+        <Route path='/' element={<SessionBooking />} />
         <Route path='/booking-confirmed' element={<BookingConfirmed />} />
+        <Route path='/session-feedback' element={<SessionFeedback />} />
+        <Route path='/my-sessions' element={<MySessions />} />
       </Routes>
     </BrowserRouter>
   );
