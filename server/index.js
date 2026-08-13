dec6ad510882473fc5ebcefa6807de39fca8360d
savinduakash <savinduvishalya@gmail.com>
2026-08-13 06:24:16 +0530

    chore: formatting index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const app = express();
 
 // ── Middleware ──
+
 // Set CORS to allow ALL origins during testing
 app.use(cors({
   origin: '*',
@@ -15,7 +16,7 @@ app.use(cors({
 
 app.use(express.json());
 
-// ── Routes ──
+// ── Routes ── 
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const mentorRoutes = require('./routes/mentorRoutes');
